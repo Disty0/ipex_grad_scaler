e138dc4090d35d743c92870c89db68269b62f83c
Disty0 <47277141+Disty0@users.noreply.github.com>
2023-06-15 01:01:06 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from .autocast_mode import autocast
+from .grad_scaler import GradScaler
+import intel_extension_for_pytorch
+
+
+def get_autocast_xpu_dtype():
+    return intel_extension_for_pytorch._C.get_autocast_xpu_dtype()
+
+
+def is_autocast_xpu_enabled():
+    return intel_extension_for_pytorch._C.is_autocast_xpu_enabled()
+
+
+def set_autocast_xpu_enabled(enabled):
+    return intel_extension_for_pytorch._C.set_autocast_xpu_enabled(enabled)
+
+
+def set_autocast_xpu_dtype(dtype):
+    return intel_extension_for_pytorch._C.set_autocast_xpu_dtype(dtype)
