a5b74bfcd2cbea4f39f00785cc4ead7b21f2ea0e
Disty0 <47277141+Disty0@users.noreply.github.com>
2023-06-15 01:07:42 +0300

    Add files via upload

---
+++ patch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir patch
+cd patch
+wget https://raw.githubusercontent.com/Disty0/ipex_grad_scaler/main/__init__.py
+wget https://raw.githubusercontent.com/Disty0/ipex_grad_scaler/main/grad_scaler.py
+mv __init__.py ../venv/lib/python3.10/site-packages/intel_extension_for_pytorch/xpu/amp/__init__.py
+mv grad_scaler.py ../venv/lib/python3.10/site-packages/intel_extension_for_pytorch/xpu/amp/grad_scaler.py
+cd ..
+rmdir patch
